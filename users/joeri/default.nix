a7dbe57b19159d9a5b4385944d0ca194722f6d8d
Joeri G <3677joeri@gmail.com>
2025-07-24 00:37:27 +0200

    Removed unused SSH key

--- users/joeri/default.nix
+++ users/joeri/default.nix
@@ -25,9 +25,6 @@
       "kvm"
       "qemu-libvirtd"
     ];
-    openssh.authorizedKeys.keys = [
-        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCge/8Xg9tN2vZ7Bh1miBZOodOtQ1P742Uf6Pb3N5Jy6phyYYR+SY3jOt+M/BDUAHWnTMS2hc8lzZhI3/mwyXqX/rIDqQUCCBS4m64ESZVEv6VarYi2ZDuPWwg/35v1LaPAtNzYEuf2wkiHhQ3RcGABsFBhWPjJswjdg0hIOT2vrILOW7Rze/wfvdy7OJIAIpJJdiIB1VK3ABQXOiZcEUbv9CigQuEptGIdXKIDMuMjthsFQiW1B9GY76lccDP4T8qPwSJNVuiGOXis7/nmdR+LD0B0GXEHJ/vno2Cm3wzI840WoU66eD/9uqwYgnXvXQvTRl+4h8TEVErKdLaKx0urkhNPyPrP4iQsIodmCqSbXGzSGP3k9vbC7dOnCsJeJjJ3wnsUjU3SM3NUr3vSjXqScHNK08bGYmC/yQMBDavVE2Zo4g2Hs65m+dULEH6beDeJ+cKDPefr0hCtmv5wndSavT/OVgwhKrsQ3Q7SwJKXV6Uv0M7C+LPuxRokiVtPavc= joeri@Joeri-Laptop"
-    ];
     packages = [inputs.home-manager.packages.${pkgs.system}.default];
   };
   home-manager.users.joeri = import ./home.nix;
