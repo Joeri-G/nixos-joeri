8fbc8a5c243d2ede237cb70c70598a99281582aa
Joeri G <3677joeri@gmail.com>
2025-07-24 16:10:39 +0200

    updated boot config

--- hosts/fossbox/configuration.nix
+++ hosts/fossbox/configuration.nix
@@ -8,9 +8,14 @@
   ];
 
   # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
+  boot.loader = {
+    systemd-boot = {
+      enable = true;
+    };
+    efi = {
+      canTouchEfiVariables = true;
+    };
+  };
 
   networking.hostName = "fossbox"; # Define your hostname.
   # networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
