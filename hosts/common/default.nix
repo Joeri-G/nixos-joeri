9f384c73b096252a4dbd8b2625d0f6dbf0815ab1
Joeri G <3677joeri@gmail.com>
2025-07-29 14:11:15 +0200

    fixed bad nix path

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -55,7 +55,6 @@
     registry =
       (lib.mapAttrs (_: flake: {inherit flake;}))
       ((lib.filterAttrs (_: lib.isType "flake")) inputs);
-    nixPath = ["/etc/nix/path"];
   };
   
   users.defaultUserShell = pkgs.fish;
