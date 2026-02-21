33aaf48e8129c895fc95365432b4a43093f69428
Joeri G <3677joeri@gmail.com>
2026-02-21 19:06:40 +0100

    moved codium to home manager

--- home/modules/vscodium.nix
+++ home/modules/vscodium.nix
@@ -1,14 +1,52 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    (vscode-with-extensions.override {
-      vscode = vscodium;
-      vscodeExtensions = with vscode-extensions; [
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscodium;
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
         jnoortheen.nix-ide
         ms-vscode.live-server
-      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [ ];
+      userSettings = {
+        nix = {
+          "serverPath" = "nixd";
+          "enableLanguageServer" = true;
+          "formatterPath" = "nixfmt";
+        };
+        "nixpkgs" = {
+            "expr" ="import <nixpkgs> { }";
+          };
+        "formatting" = {
+          "command" = [
+            "nixfmt"
+          ];
+        };
+        "update.showReleaseNotes" = false;
+
+        "window.autoDetectColorScheme" = true;
+        "workbench.preferredLightColorTheme" = "Default Light+";
+
+        git = {
+          "autofetch" = true;
+          "confirmSync" = false;
+        };
+      };
+      keybindings = [
+        {
+          key = "ctrl+tab";
+          command = "workbench.action.nextEditor";
+        }
+        { 
+          key = "ctrl+shift+tab";
+          command = "workbench.action.previousEditor";
+        }
       ];
-    })
+    };
+  };
+
+  home.packages = with pkgs; [
+    nixd # nix language server
   ];
 }
