9779f2a88d6f9634fd11a92679ef08bf1de44cb0
Joeri G <3677joeri@gmail.com>
2026-02-22 15:29:26 +0100

    Hyprlock restart script

--- home/modules/wayland/hyprlock.nix
+++ home/modules/wayland/hyprlock.nix
@@ -1,7 +1,13 @@
-{ config, pkgs, ... }:
-
+{ pkgs, ... }:
+let
+  scripts = ../../../resources/scripts;
+in
 {
   programs.hyprlock = {
     enable = true;
   };
+
+  home.packages = [
+    (pkgs.writeShellScriptBin "restart-hyprlock" (builtins.readFile "${scripts}/restart-hyprlock.sh"))
+  ];
 }
