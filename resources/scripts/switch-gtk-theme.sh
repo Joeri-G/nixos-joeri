69cf26e2bfc73b881887176bae15208c71275eee
Joeri G <3677joeri@gmail.com>
2026-02-23 01:38:36 +0100

    Added loggin

--- resources/scripts/switch-gtk-theme.sh
+++ resources/scripts/switch-gtk-theme.sh
@@ -3,5 +3,7 @@ if [[ "$1" != "dark" && "$1" != "light" ]]; then
     echo "pick light or dark"
     exit 1
 fi
+echo switching theme...
 dconf write "/org/gnome/desktop/interface/gtk-theme" "'Zukitre-$1'"
 dconf write "/org/gnome/desktop/interface/color-scheme" "'prefer-$1'"
+echo Switched to $1 theme
