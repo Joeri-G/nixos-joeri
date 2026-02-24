04deaae0209f521615e269fda2df3757feb66acd
Joeri G <3677joeri@gmail.com>
2026-02-24 02:53:53 +0100

    Updated order of transitions. I think the hyprland reload was messing with swww

--- resources/scripts/wallpaper-select.sh
+++ resources/scripts/wallpaper-select.sh
@@ -4,6 +4,8 @@ darkman_value=$(darkman get)
 # Use the value from $2 if it is provided, otherwise use darkman_value
 theme_state="${2:-$darkman_value}"
 
+echo "generating theme"
+
 if [[ "$theme_state" == "dark" ]]; then
     wal -i "$1" --cols16 lighten -n
 elif [[ "$theme_state" == "light" ]]; then
@@ -13,6 +15,8 @@ else
     exit
 fi
 
-hyprctl reload config-only # load new theme
+echo "updating image and theme"
 
 swww img --transition-fps 144 --transition-type grow --transition-duration 2 --invert-y --transition-pos 0,0 "$1"
+
+hyprctl reload config-only # load new theme
